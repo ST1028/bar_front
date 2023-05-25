9ca26e950934c65e8824c00bece10a89ecfdd746
ShounTada <108639353+ShounTada@users.noreply.github.com>
2023-05-25 22:59:47 +0900

    Bottomが被ってた対応

--- components/layouts/Footer.tsx
+++ components/layouts/Footer.tsx
@@ -12,7 +12,7 @@ export default function LabelBottomNavigation() {
   const [value, setValue] = React.useState(router.pathname.slice(1) || "/");
 
   return (
-    <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
+    <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0, zIndex: 999 }} elevation={3}>
         <BottomNavigation
           value={value}
           onChange={async (event, newValue) => {
